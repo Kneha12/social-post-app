56359ebdb3b87dc6536573d0b0365582a16ea93e
Neha Kumari <117574008+Kneha12@users.noreply.github.com>
2024-06-25 02:31:36 +0530

    first commit

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,153 @@
+import { useContext, useRef } from "react";
+import { PostList } from "../store/post-list-store";
+import { useNavigate } from "react-router-dom";
+
+const CreatePost = () => {
+  const { addPost } = useContext(PostList);
+  const navigate = useNavigate();
+
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const likesElement = useRef();
+  const dislikesElement = useRef();
+  const tagsElement = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    // const reactions = reactionsElement.current.value;
+    const likes = parseInt(likesElement.current.value) || 0;
+    const dislikes = parseInt(dislikesElement.current.value) || 0;
+    const tags = tagsElement.current.value.split(" ");
+
+    userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    // reactionsElement.current.value = "";
+    likesElement.current.value = "";
+    dislikesElement.current.value = "";
+    tagsElement.current.value = "";
+
+    fetch("https://dummyjson.com/posts/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+        reactions: { likes, dislikes },
+
+        userId: userId,
+        tags: tags,
+      }),
+    })
+      .then((res) => res.json())
+      .then((post) => {addPost(post);
+        navigate("/");
+  });
+  
+  };
+
+  return (
+    <form className="create-post" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Enter your User Id here
+        </label>
+        <input
+          type="text"
+          ref={userIdElement}
+          className="form-control"
+          id="userId"
+          placeholder="Your User Id"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          ref={postTitleElement}
+          className="form-control"
+          id="title"
+          placeholder="How are you feeling today..."
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          ref={postBodyElement}
+          rows="4"
+          className="form-control"
+          id="body"
+          placeholder="Tell us more about it"
+        />
+      </div>
+
+      {/* <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of reactions
+        </label>
+        <input
+          type="text"
+          ref={reactionsElement}
+          className="form-control"
+          id="reactions"
+          placeholder="How many people reacted to this post"
+        />
+      </div> */}
+      <div className="mb-3">
+        <label htmlFor="likes" className="form-label">
+          Number of likes
+        </label>
+        <input
+          type="number"
+          ref={likesElement}
+          className="form-control"
+          id="likes"
+          placeholder="Number of likes"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="dislikes" className="form-label">
+          Number of dislikes
+        </label>
+        <input
+          type="number"
+          ref={dislikesElement}
+          className="form-control"
+          id="dislikes"
+          placeholder="Number of dislikes"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your hashtags here
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="tags"
+          ref={tagsElement}
+          placeholder="Please enter tags using space"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </form>
+  );
+};
+
+export default CreatePost;
